ab2350990658baae89a898adbff3bbd6af8373f9
alt2razzie <alt2razzie@gmail.com>
2026-03-18 06:24:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function fetchLiveLesson(topicName) {
 
     try {
         // Send the request to your local Node.js server
-        const response = await fetch('http://localhost:3000/api/get-lesson', {
+        const response = await fetch('http://127.0.0.1:3000/api/get-lesson', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ topic: topicName })
